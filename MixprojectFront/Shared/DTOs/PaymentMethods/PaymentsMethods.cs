1b898ed8f7180b093fe5b90c2583ce1fe9f0d2f6
KarinaVilla01 <104106557+KarinaVilla01@users.noreply.github.com>
2024-01-25 15:51:18 -0700

    Primera parte de proceso de pago.

---
+++ MixprojectFront/Shared/DTOs/PaymentMethods/PaymentsMethods.cs
@@ -0,0 +1,13 @@
+﻿using MixprojectFront.Shared.DTOs.Transactions;
+
+namespace MixprojectFront.Shared.DTOs.PaymentMethods
+{
+    public class PaymentMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Transaction>? Transactions { get; set; }
+
+    }
+}
---
+++ MixprojectFront/Shared/DTOs/Transactions/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using MixprojectFront.Shared.DTOs.PaymentMethods;
+
+namespace MixprojectFront.Shared.DTOs.Transactions
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public int PaymentMethodId { get; set; }
+        public float Amount { get; set; }
+        public DateTime Date { get; set; }
+        public Guid IdClient { get; set; }
+
+        public PaymentMethod? PaymentMethod { get; set; }
+
+    }
+}
